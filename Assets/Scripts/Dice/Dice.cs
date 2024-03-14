2a15b19690b76a98cd3d48baea9ad680252c2470
jiyongjin <l1110hj@gmail.com>
2024-03-15 02:26:01 +0900

    Remade Bouncing Dice && rolling the dice

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -5,52 +5,54 @@ using DG.Tweening;
 
 public class Dice : MonoBehaviour
 {
-    [SerializeField]
-    private float maxScale = 1.2f;
-    [SerializeField]
-    private float minScale = .5f;
-    [SerializeField]
-    //private float durationPart1 = .5f;
 
-    private float shrinkSpeed = .15f;
-    private float w;
+    [Header("BouncingDice")]
+    [SerializeField]
+    private float maxHeight = 1.5f;      // 최대 높이(크기)
+    private float height = 0f;           // 현재 높이(크기)
+    private float acceleration = 0f;     // 가속도값
+    private float bouncingCount = 0f;    // 튕기는 최대 값
 
     private void Start()
     {
-        w = maxScale;
+        height = maxHeight;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKey(KeyCode.Space))
         {
-            SizeUP();
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            BounceDice();
-            w = maxScale;
-        }
-    }
+            height = maxHeight;
 
-    private void SizeUP()
-    {
-        transform.localScale = Vector3.one * minScale;
-        transform.DOScale(maxScale, 0.05f);
-    }
+            transform.localScale = Vector3.one * height;
 
-    private void BounceDice()
-    {
-        transform.DOScale(minScale, shrinkSpeed).OnComplete(() =>
+            acceleration = 0;
+        }
+        else
         {
-            w /= 1.18f;
-            if (w > 0.6f)
+            acceleration -= Time.deltaTime * 0.2f; // 변경된 가속도 값
+
+            height += acceleration;
+
+            if (height <= .6)
             {
-                transform.DOScale(w, shrinkSpeed).OnComplete(() =>
-                {
-                    BounceDice();
-                });
+                // 가속도값에 -곱하여 +로 만듬
+                acceleration = (acceleration / 1.12f) * -1;
+                height = .6f;
+                bouncingCount += 1;
+                Debug.Log(bouncingCount);
             }
-        });
+
+            if (Mathf.Abs(acceleration) <= 0.013f && Mathf.Abs(height) <= .7f || bouncingCount >= 5) // 변경된 값
+            {
+                acceleration = 0f;
+                height = .7f;
+                bouncingCount = 0;
+            }
+
+            transform.localScale = Vector3.one * height;
+
+        }
     }
+
 }
