1a9573ed88f6d58289fb4bf11f917aeb19114d81
jiyongjin <l1110hj@gmail.com>
2024-04-16 09:09:55 +0900

    #2 Dice Choose & DIce Shadow

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -5,97 +5,6 @@ using System;
 
 public class Dice : MonoBehaviour
 {
-    //[Header("BouncingDice")]
-    //[SerializeField]
-    //public float maxHeight = 1.5f;      // 최대 높이(크기)
-    //private float height = 0f;           // 현재 높이(크기)
-    //private float acceleration = 0f;     // 가속도값
-    //private float bouncingCount = 0f;    // 튕기는 최대 값
-
-    //[Header("RollingDice")]
-    //[SerializeField] private Sprite[] diceSides; // 주사위 스프라이트들
-    //private SpriteRenderer rend;                 // 스프라이트렌더러
-    //private int randomDiceSides = 0;             // 1 ~ 5 랜덤
-    //private int finalSide = 0;                   // 마지막 결과값
-    //private bool loringCheck;
-
-    //public event Action<float> HeightChanged; // 현재 높이값 변경 이벤트
-
-    //private bool isHolding;
-    //private bool Doubleclickprevention = true;
-
-    //private void Start()
-    //{
-    //    rend = GetComponent<SpriteRenderer>();
-    //}
-
-    //private void Update()
-    //{
-
-    //    if (DiceManager.instance.isDragging)
-    //    {
-    //        isHolding = true;
-    //    }
-
-    //    if (isHolding)
-    //    {
-    //        if (Input.GetMouseButton(0) && Doubleclickprevention)
-    //        {
-    //            height = maxHeight;
-    //            transform.localScale = Vector3.one * height;
-    //            acceleration = 0;
-    //            loringCheck = true;
-    //        }
-    //        else
-    //        {
-    //            Doubleclickprevention = false;
-
-    //            acceleration -= Time.deltaTime * 0.2f; 
-    //            height += acceleration;  // 높이값에 가속력값 계속 더하기
-
-    //            if (height <= .6)
-    //            {
-    //                // 가속도값에 -곱하여 +로 만듬
-    //                acceleration = (acceleration / 1.12f) * -1;
-    //                height = .6f;
-    //                bouncingCount += 1;
-
-    //                // 주사위 눈 바꾸기
-    //                randomDiceSides = UnityEngine.Random.Range(0, 6);
-    //                rend.sprite = diceSides[randomDiceSides];
-    //            }
-
-
-
-    //            if (Mathf.Abs(acceleration) <= 0.013f && Mathf.Abs(height) <= .7f || bouncingCount >= 5)
-    //            {
-    //                acceleration = 0f;
-    //                height = .7f;
-    //                bouncingCount = 0;
-
-    //                if (loringCheck)
-    //                {
-    //                    finalSide = randomDiceSides + 1;
-
-    //                    Debug.Log(finalSide);
-    //                    loringCheck = false;
-    //                    isHolding = false;
-    //                    Doubleclickprevention = true;
-    //                }
-    //            }
-
-    //            transform.localScale = Vector3.one * height;
-
-    //            HeightChanged?.Invoke(height);
-    //        }
-    //    }
-    //} 
-
-
-
-
-
-
     [Header("BouncingDice")]
     [SerializeField]
     public float maxHeight = 1.5f;      // 최대 높이(크기)
@@ -105,103 +14,105 @@ public class Dice : MonoBehaviour
 
     [Header("RollingDice")]
     public Transform dice;
-    [SerializeField] private Sprite[] diceSides; // 주사위 스프라이트들
-    private SpriteRenderer rend;                 // 스프라이트렌더러
-    private int randomDiceSides = 0;             // 1 ~ 6 랜덤
-    private int finalSide = 0;                   // 마지막 결과값
+    public Sprite[] diceSides; // 주사위 스프라이트들
+    [HideInInspector]
+    public SpriteRenderer rend;                 // 스프라이트렌더러
+    public int randomDiceSides = 0;             // 1 ~ 6 랜덤
+    public int finalSide = 0;                   // 마지막 결과값
 
     public event Action<float> HeightChanged; // 현재 높이값 변경 이벤트
 
-    public bool W = false;
-    public bool coliderCheck = false;
+    public bool SelectedDice = false;
+    public bool BounceTime = false; // 드레그중 손 땠을때 부터 바운스가 멈출때까지
     private bool isHolding;
-    private bool Doubleclickprevention = true; // 튕기는 도중 다시 잡을수 없게
 
     public bool endLoring = true;
+    public bool reRollCheck = false;
 
     private void Start()
     {
         rend = dice.GetComponent<SpriteRenderer>();
-       
+
     }
 
     private void OnMouseOver()
     {
-        if (W) return;
+        if (SelectedDice) return;
 
         if (Input.GetMouseButtonDown(1) && endLoring) // 우클릭 검사
         {
-            // 여기서 DiceManager의 diceRollingList에서 해당 오브젝트를 제거하는 코드 추가
+            // 여기서 DiceManager의 diceRollingList에서 해당 오브젝트를 제거하는 코드
             DiceManager.instance.diceRollingList.Remove(GetComponent<Rigidbody2D>());
             DiceManager.instance.selectedDice.Add(GetComponent<Dice>());
-            W = true;
-            coliderCheck = true;
+            SelectedDice = true;
         }
     }
 
     private void Update()
     {
 
-        if (W) return;
+        if (SelectedDice) return;
 
         if (DiceManager.instance.isDragging)
         {
             isHolding = true;
         }
 
-        if (isHolding)
+        StartCoroutine(RollingDice());
+
+    }
+
+    private IEnumerator RollingDice()
+    {
+        yield return new WaitUntil(() => isHolding);
+
+        endLoring = false;
+        if (Input.GetMouseButton(0) && !BounceTime) // 마우스를 클릭하고있을때 && 주사위가 튕기던 도중 다시 잡을수없도록함
         {
-            endLoring = false;
-            if (Input.GetMouseButton(0) && Doubleclickprevention) // 마우스를 클릭하고있을때 && 주사위가 튕기던 도중 다시 잡을수없도록함
+            height = maxHeight;
+            dice.localScale = Vector3.one * height;
+            acceleration = 0;
+        }
+        else // 마우스를 땠을때
+        {
+            
+            BounceTime = true;
+
+            acceleration -= Time.deltaTime * 0.2f;
+            height += acceleration;  // 높이값에 가속력값 계속 더하기
+
+            if (height <= .6) // 튕기는 이벤트
             {
-                height = maxHeight;
-                dice.localScale = Vector3.one * height;
-                acceleration = 0;
+                // 가속도값에 -곱하여 +로 만듬
+                acceleration = (acceleration / 1.12f) * -1;
+                height = .6f;
+                bouncingCount += 1;
+
+                // 주사위 눈 바꾸기
+                randomDiceSides = UnityEngine.Random.Range(0, 6);
+                rend.sprite = diceSides[randomDiceSides];
             }
-            else // 마우스를 땠을때
+
+
+
+            if (Mathf.Abs(acceleration) <= 0.013f && Mathf.Abs(height) <= .7f || bouncingCount >= 5)  // 가속도값이 0에 가까움 && 높이가 0.7보다 작거나 같음 || 튕긴 횟수가 5보다 크거나 같을경우
             {
-                Doubleclickprevention = false;
+                acceleration = 0f;
+                height = .7f;
+                bouncingCount = 0;
 
-                acceleration -= Time.deltaTime * 0.2f;
-                height += acceleration;  // 높이값에 가속력값 계속 더하기
+                finalSide = randomDiceSides + 1;
 
-                if (height <= .6) // 튕기는 이벤트
-                {
-                    // 가속도값에 -곱하여 +로 만듬
-                    acceleration = (acceleration / 1.12f) * -1;
-                    height = .6f;
-                    bouncingCount += 1;
-
-                    // 주사위 눈 바꾸기
-                    randomDiceSides = UnityEngine.Random.Range(0, 6);
-                    rend.sprite = diceSides[randomDiceSides];
-                }
-
-
-
-                if (Mathf.Abs(acceleration) <= 0.013f && Mathf.Abs(height) <= .7f || bouncingCount >= 5)  // 가속도값이 0에 가까움 && 높이가 0.7보다 작거나 같음 || 튕긴 횟수가 5보다 크거나 같을경우
-                {
-                    acceleration = 0f;
-                    height = .7f;
-                    bouncingCount = 0;
-
-
-                    finalSide = randomDiceSides + 1;
-
-                    Debug.Log(finalSide);
-                    isHolding = false;
-                    Doubleclickprevention = true;
-                    endLoring = true;
-                }
-
-                dice.localScale = Vector3.one * height;
-
-                HeightChanged?.Invoke(height);
+                Debug.Log(finalSide);
+                isHolding = false;
+                BounceTime = false;
+                endLoring = true;
             }
+
+            dice.localScale = Vector3.one * height;
+
+            HeightChanged?.Invoke(height);
         }
     }
 
-
-    //이제 우클릭 하면 Bool w값과 DiceManager의 값을 여기서 빼고 하면 되는거
-    //이제 크기가 다작아져야지 우클릭이나 다시 키우기나 다시 시작 등을 할수있다를 넣으면됨
 }
--- Assets/Scripts/Dice/DiceManager.cs
+++ Assets/Scripts/Dice/DiceManager.cs
@@ -1,107 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DiceManager : MonoBehaviour
 {
-    //public List<Rigidbody2D> diceRollingList = new List<Rigidbody2D>();
+    public List<Dice> diceList = new List<Dice>();   // 턴 엔드나 다시 돌릴거 없을때 여기꺼 받아오면 되쟈나? 나 ㅄ인가?
+    public List<Rigidbody2D> diceRollingList = new List<Rigidbody2D>();   //
+    public List<Dice> selectedDice = new List<Dice>();                    // 우클릭시 이 두 리스트의 관계는 Dice.cs에 있음
 
-    //private Vector3 targetPosition;
-    //public bool isDragging = false;
-    //private float moveSpeed = 20f;
-    //private float throwSpeed = 35f;
-    //private float deceleration = 1.7f; // 감속도
-
-    //public bool isRollingDice = true;
-
-    //public static DiceManager instance { get; private set; }
-
-    //private void Awake()
-    //{
-    //    instance = this;
-    //}
-
-    //private void Update()
-    //{
-    //    if (isRollingDice)
-    //    {
-    //        if (Input.GetMouseButton(0))
-    //        {
-    //            isDragging = true;
-    //        }
-
-    //        if (Input.GetMouseButtonUp(0))
-    //        {
-    //            isDragging = false;
-    //            RollingDice();
-    //            isRollingDice = false;
-    //        }
-    //    }
-
-    //    if (isDragging)
-    //    {
-    //        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //        targetPosition.z = 0f;
-    //        foreach (Rigidbody2D rb in diceRollingList)
-    //        {
-    //            Vector3 moveDirection = (targetPosition - rb.transform.position).normalized;
-    //            rb.velocity = moveDirection * moveSpeed;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        // 속도감속
-    //        foreach (Rigidbody2D rb in diceRollingList)
-    //        {
-    //            rb.velocity -= rb.velocity.normalized * deceleration * Time.deltaTime;
-    //            rb.angularVelocity *= Mathf.Clamp01(1f - deceleration * Time.deltaTime);
-    //        }
-    //    }
-
-    //    if(Input.GetKeyDown(KeyCode.R) && !isRollingDice)
-    //    {
-    //        isRollingDice = true;
-    //    }
-    //}
-
-    //private void RollingDice()
-    //{
-    //    foreach (Rigidbody2D dice in diceRollingList)
-    //    {
-    //        dice.velocity = Vector2.zero;
-    //        dice.angularVelocity = Random.Range(-360f, 360f);
-    //        dice.AddForce(Random.insideUnitCircle * throwSpeed, ForceMode2D.Impulse);
-    //    }
-    //}
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (isDragging)
-    //    {
-    //        foreach (Rigidbody2D rb in diceRollingList)
-    //        {
-    //            rb.velocity = Vector2.zero;
-    //        }
-    //    }
-    //}
-
-
-
-
-
-
-
-    public List<Dice> diceList = new List<Dice>();
-    [SerializeField]
-    public List<Rigidbody2D> diceRollingList = new List<Rigidbody2D>();
-
-    public List<Dice> selectedDice = new List<Dice>();
-
-    public int rerollNum = 4; 
+    public int rerollNum = 4;
+    public TMP_Text RollingNumText;
     private Vector3 targetPosition;
     public bool isDragging = false;
-    private float moveSpeed = 20f;
-    private float throwSpeed = 35f;
+    private float moveSpeed = 30f;
+    private float throwSpeed = 38f;
     private float deceleration = 1.7f; // 감속도
 
     public bool isRollingDice = true;
@@ -121,6 +34,7 @@ public class DiceManager : MonoBehaviour
 
     private void Update()
     {
+        RollingNumText.text = "X " + rerollNum.ToString();
         dragUpDice();
 
         if (isRollingDice)
@@ -165,7 +79,12 @@ public class DiceManager : MonoBehaviour
                 isRollingDice = true;
                 rerollNum -= 1;
             }
-        } 
+        }
+
+        //if (rerollNum <= 0) // 코루틴으로 바꿀만함
+        //{
+        //    StartCoroutine(DiceEndMove());
+        //}
 
         if (Input.GetKeyDown(KeyCode.L))
         {
@@ -187,13 +106,48 @@ public class DiceManager : MonoBehaviour
     private void IsMyTurn()
     {
         diceRollingList.Clear();
+        selectedDice.Clear();
+        rerollNum = 4;
+
+        for (int n = 0; n < diceList.Count; n++)
+        {
+            Vector3 StartDicePos = Vector3.zero;
+            Quaternion targetRotation = Quaternion.identity;
+            float StartDiceScale = 1.2f;
+
+            switch (n)
+            {
+                case 0:
+                    StartDicePos = new Vector3(-3.32f, -2, 0f);
+                    break;
+                case 1:
+                    StartDicePos = new Vector3(-1.68f, -2, 0f);
+                    break;
+                case 2:
+                    StartDicePos = new Vector3(-0.04f, -2, 0f);
+                    break;
+                case 3:
+                    StartDicePos = new Vector3(1.58f, -2, 0f);
+                    break;
+                case 4:
+                    StartDicePos = new Vector3(3.22f, -2, 0f);
+                    break;
+            }
+
+            diceList[n].transform.position = StartDicePos; // 초반 위치
+            diceList[n].transform.rotation = targetRotation; // 초반 회전도
+            diceList[n].dice.transform.localScale = Vector3.one * StartDiceScale; // 초반 크기
+
+            diceList[n].dice.GetComponent<SpriteRenderer>().sprite = diceList[n].diceSides[0]; //오브젝트 1로 바꾸기
+            diceList[n].endLoring = false;
+        }
 
         foreach (var n in diceList)
         {
             diceRollingList.Add(n.GetComponent<Rigidbody2D>());
             for (int i = 0; i < 5; i++)
             {
-                diceList[i].W = false;
+                diceList[i].SelectedDice = false;
             }
         }
     }
@@ -210,19 +164,19 @@ public class DiceManager : MonoBehaviour
             switch (i)
             {
                 case 0:
-                    targetPosition = new Vector3(-3.25f, 2.36f, 0f);
+                    targetPosition = new Vector3(-3.32f, 2.36f, 0f);
                     break;
                 case 1:
-                    targetPosition = new Vector3(-1.65f, 2.36f, 0f);
+                    targetPosition = new Vector3(-1.68f, 2.36f, 0f);
                     break;
                 case 2:
-                    targetPosition = new Vector3(0f, 2.36f, 0f);
+                    targetPosition = new Vector3(-0.04f, 2.36f, 0f);
                     break;
                 case 3:
-                    targetPosition = new Vector3(1.65f, 2.36f, 0f);
+                    targetPosition = new Vector3(1.58f, 2.36f, 0f);
                     break;
                 case 4:
-                    targetPosition = new Vector3(3.25f, 2.36f, 0f);
+                    targetPosition = new Vector3(3.22f, 2.36f, 0f);
                     break;
             }
 
@@ -230,12 +184,14 @@ public class DiceManager : MonoBehaviour
             selectedDice[i].transform.position = Vector3.MoveTowards(selectedDice[i].transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
             // 주사위 크기 조절
-            selectedDice[i].dice.transform.localScale = Vector3.one * Mathf.MoveTowards(selectedDice[i].transform.localScale.x, targetScale, moveSpeed * Time.deltaTime);
+            selectedDice[i].dice.transform.localScale = Vector3.one * Mathf.MoveTowards(selectedDice[i].dice.transform.localScale.x, targetScale, moveSpeed * Time.deltaTime);
 
             // 주사위 회전
             selectedDice[i].transform.rotation = Quaternion.RotateTowards(selectedDice[i].transform.rotation, targetRotation, moveSpeed * Time.deltaTime * 30);
 
-            selectedDice[i].W = true;
+            selectedDice[i].SelectedDice = true;
+
+            //Debug.Log(selectedDice[i].finalSide);
         }
     }
 
@@ -246,6 +202,19 @@ public class DiceManager : MonoBehaviour
             foreach (Rigidbody2D rb in diceRollingList)
             {
                 rb.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    private IEnumerator DiceEndMove()
+    {
+        foreach (Dice dice in diceList)
+        {
+            if (!dice.endLoring) // 선택되지 않은 주사위만 선택된 주사위로 이동
+            {
+                selectedDice.Add(dice);
+                dice.endLoring = true;
+                yield return new WaitForSeconds(0.1f);
             }
         }
     }
